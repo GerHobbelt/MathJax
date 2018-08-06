0c2d3ab5c994a19f6dab83fc14d87684470c096d
Ger Hobbelt <ger@hobbelt.com>
2018-08-06 02:46:43 +0200

    fix crash due to misnamed member variable: `ExSpan` does not exist, but `EmExSpan` does!

--- unpacked/extensions/MatchWebFonts.js
+++ unpacked/extensions/MatchWebFonts.js
@@ -145,7 +145,7 @@
         for (i = 0, m = scripts.length; i < m; i++) {
           script = scripts[i];
           if (script.parentNode && script.MathJax.elementJax) {
-            script.parentNode.insertBefore(this.ExSpan.cloneNode(true),script);
+            script.parentNode.insertBefore(this.EmExSpan.cloneNode(true),script);
           }
         }
         //
