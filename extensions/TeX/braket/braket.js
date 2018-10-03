8f7435263b830ae3b856c2b681490e1236a85bd8
Ger Hobbelt <ger@hobbelt.com>
2018-10-03 16:30:06 +0200

    fix error in braket extension. Also add its original CTAN sources, etc.

--- extensions/TeX/braket/braket.js
+++ extensions/TeX/braket/braket.js
@@ -1,3 +1,21 @@
+/*
+ *  ../../../..//extensions/TeX/braket/braket.js
+ *
+ *  Copyright (c) 2009-2018 The MathJax Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 /*braket.js
  *  
  * Implements the Braket Package for LaTeX input.
@@ -43,4 +61,5 @@ MathJax.InputJax.TeX.Definitions.Add({
 MathJax.Hub.Startup.signal.Post("TeX braket Ready")}); 
 MathJax.Ajax.loadComplete("[MathJax]/extensions/TeX/braket.js");
 
-//MathJax.Ajax.loadComplete("[Contrib]/physics/unpacked/physics.js");
+//MathJax.Ajax.loadComplete("[Contrib]/physics/physics.js");
+
--- unpacked/extensions/TeX/brakets/README.md
+++
@@ -1,6 +0,0 @@
-# mathjax-brakets
-
-MathJax Support for the LaTeX package "braket", a simple package generally used by physicists to typeset notation 
-commonly seen in quantum mechanics. Additionally, set notation is also implemented.
-
-Allows programmers to write quantum-mechanical equations without the overhead of additional or unnecessary macros
