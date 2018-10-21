3a360fcafb77d1a4a48c9d978e5fe624f0f916eb
Ger Hobbelt <ger@hobbelt.com>
2018-10-21 17:42:13 +0200

    typo fix

--- unpacked/jax/output/SVG/jax.js
+++ unpacked/jax/output/SVG/jax.js
@@ -2190,7 +2190,7 @@ console.log("Glyph: ", {
       toSVG: function (span,div) {
         var CONFIG = SVG.config;
         //
-        //  All the data should be in an inferrerd row
+        //  All the data should be in an inferred row
         //
         if (this.data[0]) {
           this.SVGgetStyles();
