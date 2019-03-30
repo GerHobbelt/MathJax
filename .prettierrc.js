a7debf0d56bfcf461c441ac2bbdd4377fbd627d8
Ger Hobbelt <ger@hobbelt.com>
2019-03-30 17:09:09 +0100

    copying the eslint+prettier settings from my ASCIImathML project.

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "arrowParens": "always",
+
+  //"trailingComma": "es5",
+  "trailingComma": "none",
+  //"trailingComma": "all",
+  
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": false,
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "tabWidth": 2,
+  "printWidth": 120,
+};
